14d6eb7855007de43f15c173943cedb9ebc753be
Ruslan Mustafin <ruslanmust87@gmail.com>
2018-01-07 16:59:24 +0500

    tw_1

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,13 @@
+import { patch } from '../../Users/user/AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/semver';
+
+const path = require('path');
 const webpack = require('webpack'); 
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const PATHS = {
+  source: path.join(__dirname, 'src'),
+  build: path.join(__dirname, 'dest')
+}
 
 const config = {
   entry: {
@@ -7,14 +15,15 @@ const config = {
     'blog': './src/scripts/blog.js'
   },
   output: {
-    filename: '[name].js'
+    filename: 'script/[name].js'
   },
   plugins: [
     new UglifyJSPlugin({
       sourceMap: true
     }),
     new webpack.ProvidePlugin({
-      $: 'jquery'
+      $: 'jquire',
+      jQuery: 'jquery'
     })
   ]
 };
