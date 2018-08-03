fe0946372fa67efd3a46cf353266ef515f7d99bd
Ruslan Mustafin <ruslanmust87@gmail.com>
2018-08-03 18:12:32 +0500

    blur_3

--- src/pages/portfolio/portfolio.js
+++ src/pages/portfolio/portfolio.js
@@ -26,17 +26,38 @@ hamburger();
 
 // позиционирование фона для элемента с блюром
 
-function blur(){
-	const section = document.querySelector('.portfolio-contact');
-	const elem = document.querySelector('.portfolio-contact__form');
-	var blurElem = document.querySelector('.portfolio-contact__form-blur');
-	const elemHeight = section.clientHeight;
-	const elemWidth = section.clientWidth;
-	const posLeft = -elem.offsetLeft;
-	const posTop = -elem.offsetTop;
+// function blur(){
+// 	const section = document.querySelector('.portfolio-contact');
+// 	const elem = document.querySelector('.portfolio-contact__form');
+// 	var blurElem = document.querySelector('.portfolio-contact__form-blur');
+// 	const elemHeight = section.clientHeight;
+// 	const elemWidth = section.clientWidth;
+// 	const posLeft = -elem.offsetLeft;
+// 	const posTop = -elem.offsetTop;
 
-	blurElem.style.backgroundSize = `${elemWidth}px ${elemHeight}px`;
-	blurElem.style.backgroundPosition = `${posLeft}px ${posTop}px`;
+// 	blurElem.style.backgroundSize = `${elemWidth}px ${elemHeight}px`;
+// 	blurElem.style.backgroundPosition = `${posLeft}px ${posTop}px`;
+// }
+
+function blur(){
+	// const heightToWidth = 0.75;
+	const widthToHeight = 1.332; // соотношение ширины к высоте
+	const section = document.querySelector('.portfolio-contact'); // секция родитель
+	const blurElem = document.querySelector('.portfolio-contact__form-blur'); // форма
+	const form = document.querySelector('.portfolio-contact__form'); // элемент с блюром
+
+	const sectionHeight = section.clientHeight; // высота секции родителя
+	// const sectionWidth = section.clientWidth;
+	const formWidth = form.clientWidth; // ширина формы
+
+	const sectionBackgroundWidth = sectionHeight * widthToHeight; // иширина фона родителя 
+
+	const posTop = -form.offsetTop; // позиционирование по высоте
+	const posLeft = -(sectionBackgroundWidth / 2 - formWidth / 2); // позиционирование по высоте
+
+	
+	blurElem.style.backgroundSize = `auto ${sectionHeight}px`; // размеры фона элемета блюра
+	blurElem.style.backgroundPosition = `${posLeft}px ${posTop}px`; // позиционирование
 }
 
 blur();
