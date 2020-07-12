7f41b060962a2110f9ca192c64b4fcd7ec468464
Ruslan Mustafin <ruslanmust87@gmail.com>
2020-07-12 19:16:44 +0500

    create__men__title

--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -4,7 +4,9 @@ import './blog.scss';
 
 
 import preloader from '../../components/blocks/preloader/preloader';
-import {verticalParalax} from  '../../components/blocks/vertical-paralax/vertical-paralax';
+import {
+	verticalParalax
+} from '../../components/blocks/vertical-paralax/vertical-paralax';
 import hamburger from '../../components/blocks/hamburger_icon/hamburger_icon.js';
 import hamburgerAnimation from '../../components/blocks/hamburger_menu/hamburger_menu.js';
 
@@ -14,23 +16,56 @@ preloader();
 
 
 var container = document.querySelector('.blog-content__articles');
-var accoMenuItems = document.querySelectorAll('.menu-acco__item');
-var blogContentPosition; 
+var blogContentPosition;
+
+//формирование меню-аакордеон с заголовками статей
+
+let menuAcco = document.querySelector('.menu-acco');
+
+const listTitle = ['Самое важное в SASS', 'Приемы верстки, без которых не обходится не один сайт',
+	'Самые необходимый набор Webpack плагинов', 'Почему я выбрал Pug'
+];
+
+fetch('http://127.0.0.1:3001/blog', {
+	method: 'GET',
+	mode: 'cors',
+	//headers: {
+	//	'Content-Type': 'application/json; charset=utf-8',
+	//},
+	//body: body
+}).then(
+	res => {
+		for (let i = 0; i < 4; i++) {
+			let menuAccoItem = document.createElement('li');
+			let menuAccoItemDesc = document.createElement('div');
+
+			menuAccoItem.classList.add('menu-acco__item');
+			menuAccoItemDesc.classList.add('menu-acco__description');
+			menuAccoItemDesc.innerText = listTitle[i];
+
+			menuAccoItem.appendChild(menuAccoItemDesc);
+			menuAcco.appendChild(menuAccoItem);
+		}
+	}
+);
+
+menuAcco.firstElementChild.classList.add('acco__item-active');
 
 // прокрутка статей в blog-content
 
-$(accoMenuItems).on('click', function(){
+var accoMenuItems = document.querySelectorAll('.menu-acco__item');
+$(accoMenuItems).on('click', function() {
 	var indexAccoMenuItems = $(this).index();
 	blogContentPosition = $('.blog__content-item').eq(indexAccoMenuItems).position();
 	scrollContent(blogContentPosition.top);
 	toggleClass($(this));
 });
 
-function scrollContent(pos){
+function scrollContent(pos) {
 	$(container).css('top', '-' + pos + 'px');
 }
 
-function toggleClass(activeItem){
+function toggleClass(activeItem) {
 	$(accoMenuItems).removeClass('acco__item-active');
 	$(activeItem).addClass('acco__item-active');
 }
